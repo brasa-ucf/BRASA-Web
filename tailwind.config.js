59c3caa0ffb5fdd289dcf525801d6f3283dc7ff0
Pedro Fachetti Carvalho <73730027+pedrofcarvalho@users.noreply.github.com>
2022-12-24 01:20:43 -0500

    Created 'src' file (#2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
